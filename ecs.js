ee1e3a1694c7a297f212e99edfba550f2ff71175
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-03-12 19:09:47 -0400

    the actual code stuff

---
+++ ecs.js
@@ -0,0 +1,53 @@
+class SceneManager {
+  constructor(scenes, currentScene) {
+    this.scenes = scenes;
+    this.currentScene = currentScene;
+  }
+  
+  getCurrentScene() {
+    return this.scenes[this.currentScene];
+  }
+  
+  start(scene = this.getCurrentScene()) {
+    scene.start(this);
+  }
+  
+  update(scene = this.getCurrentScene()) {
+    scene.update(this);
+  }
+}
+
+class Scene {
+  constructor(objects, startScene, updateScene) {
+    this.objects = objects;
+    this.startScene = startScene;
+    this.updateScene = updateScene;
+  }
+  
+  startObjects(manager) {
+    for (let i = this.objects.head; i != undefined; i = i.next)
+      i.data.start(manager, this, i.data);
+  }
+
+  updateObjects(manager) {
+    for (let i = this.objects.head; i != undefined; i = i.next)
+      i.data.update(manager, this, i.data);
+  }
+  
+  start(manager) {
+    this.startScene(manager, this);
+    this.startObjects(manager);
+  }
+  
+  update(manager) {
+    this.updateScene(manager, this);
+    this.updateObjects(manager);
+  }
+}
+
+class GameObject {
+  constructor(start, update) {
+    this.start = start;
+    this.update = update;
+  }
+}
